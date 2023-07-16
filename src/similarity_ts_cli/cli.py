657a9aa3b85ccb5126ba6701d73efd0a392889eb
alejandrofdez-us <afdez@us.es>
2023-07-16 13:06:24 +0200

    adapted to being called differently

--- src/similarity_ts_cli/cli.py
+++ src/similarity_ts_cli/cli.py
@@ -14,7 +14,79 @@ from similarity_ts.similarity_ts import SimilarityTs
 from similarity_ts.helpers.csv_reader_helper import load_ts_from_csv, load_ts_from_path
 
 
-def main(arguments):
+def main():
+    available_metrics = MetricFactory.find_available_metrics().keys()
+    available_figures = PlotFactory.find_available_figures().keys()
+    parser = argparse.ArgumentParser(
+        usage='similarity-ts-cli -ts1 path_to_file_1 -ts2_path path_to_files_2 [--metrics] [js ...] [--figures] [tsne ...] \
+            [--header] [--timestamp_frequency_seconds] 300 [--stride] 2 [--window_selection_metric] metric_name'
+    )
+    parser.add_argument(
+        '-ts1',
+        '--time_series_1_filename',
+        help='<Required> Include a csv filename that represents a time series. If ts1 is bigger than time series in ts2_path, it will be split in windows.',
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        '-ts2',
+        '--time_series_2_path',
+        help='<Required> Include the path to a csv file or a directory with csv files, each one representing time series.',
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        '-m',
+        '--metrics',
+        nargs='+',
+        help='<Optional> Include metrics to be computed as a list separated by spaces.',
+        choices=available_metrics,
+        required=False,
+    )
+    parser.add_argument(
+        '-f',
+        '--figures',
+        nargs='+',
+        help='<Optional> Include figure names to be generated as a list separated by spaces.',
+        choices=available_figures,
+        required=False,
+    )
+    parser.add_argument(
+        '-head',
+        '--header',
+        help='<Optional> If the time-series includes a header row.',
+        required=False,
+        action='store_true',
+    )
+    parser.add_argument(
+        '-ts_freq_secs',
+        '--timestamp_frequency_seconds',
+        help='<Optional> Include the frequency in seconds in which samples were taken.',
+        required=False,
+        default=1,
+        type=int,
+    )
+    parser.add_argument(
+        '-strd',
+        '--stride',
+        help='<Optional> Include the stride to be used in moving windows over samples.',
+        required=False,
+        default=1,
+        type=int,
+    )
+    parser.add_argument(
+        '-w_select_met',
+        '--window_selection_metric',
+        help='<Optional> Include the chosen metric used to pick the best window in the first time series.',
+        required=False,
+        default='dtw',
+        type=str,
+    )
+    args = parser.parse_args()
+    __main_script(args)
+
+
+def __main_script(arguments):
     ts1, header_ts1 = load_ts_from_csv(arguments.time_series_1_filename, arguments.header)
     ts2_dict = load_ts_from_path(arguments.time_series_2_path, header_ts1, arguments.header)
     similarity_ts_config = __create_similarity_ts_config(arguments, list(ts2_dict.keys()), header_ts1)
@@ -104,72 +176,4 @@ def __save_metrics(computed_metrics, path='results/metrics'):
 
 
 if __name__ == '__main__':
-    available_metrics = MetricFactory.find_available_metrics().keys()
-    available_figures = PlotFactory.find_available_figures().keys()
-    parser = argparse.ArgumentParser(
-        usage='python cli.py -ts1 path_to_file_1 -ts2_path path_to_files_2 [--metrics] [js ...] [--figures] [tsne ...] \
-            [--header] [--timestamp_frequency_seconds] 300 [--stride] 2 [--window_selection_metric] metric_name'
-    )
-    parser.add_argument(
-        '-ts1',
-        '--time_series_1_filename',
-        help='<Required> Include a csv filename that represents a time series. If ts1 is bigger than time series in ts2_path, it will be split in windows.',
-        type=str,
-        required=True,
-    )
-    parser.add_argument(
-        '-ts2',
-        '--time_series_2_path',
-        help='<Required> Include the path to a csv file or a directory with csv files, each one representing time series.',
-        type=str,
-        required=True,
-    )
-    parser.add_argument(
-        '-m',
-        '--metrics',
-        nargs='+',
-        help='<Optional> Include metrics to be computed as a list separated by spaces.',
-        choices=available_metrics,
-        required=False,
-    )
-    parser.add_argument(
-        '-f',
-        '--figures',
-        nargs='+',
-        help='<Optional> Include figure names to be generated as a list separated by spaces.',
-        choices=available_figures,
-        required=False,
-    )
-    parser.add_argument(
-        '-head',
-        '--header',
-        help='<Optional> If the time-series includes a header row.',
-        required=False,
-        action='store_true',
-    )
-    parser.add_argument(
-        '-ts_freq_secs',
-        '--timestamp_frequency_seconds',
-        help='<Optional> Include the frequency in seconds in which samples were taken.',
-        required=False,
-        default=1,
-        type=int,
-    )
-    parser.add_argument(
-        '-strd',
-        '--stride',
-        help='<Optional> Include the stride to be used in moving windows over samples.',
-        required=False,
-        default=1,
-        type=int,
-    )
-    parser.add_argument(
-        '-w_select_met',
-        '--window_selection_metric',
-        help='<Optional> Include the chosen metric used to pick the best window in the first time series.',
-        required=False,
-        default='dtw',
-        type=str,
-    )
-    args = parser.parse_args()
-    main(args)
+    main()
