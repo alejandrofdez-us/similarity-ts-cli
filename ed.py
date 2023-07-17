2c645ca341f9323f96ff0709a4b8be6dfec26440
alejandrofdez-us <afdez@us.es>
2023-07-17 14:28:00 +0200

    updated example of euclidean distance to fit the new signature of compute

--- ed.py
+++ ed.py
@@ -6,14 +6,14 @@ class EuclideanDistance(Metric):
 
     def __init__(self):
         super().__init__()
-        self.name = 'eu'
+        self.name = 'ed'
 
-    def compute(self, ts1, ts2):
-        metric_result = {'Multivariate': self.__eu(ts1, ts2)}
+    def compute(self, ts1, ts2, similarity_ts):
+        metric_result = {'Multivariate': self.__ed(ts1, ts2)}
         return metric_result
 
     def compute_distance(self, ts1, ts2):
-        return self.__eu(ts1, ts2)
+        return self.__ed(ts1, ts2)
 
-    def __eu(self, ts1, ts2):
+    def __ed(self, ts1, ts2):
         return np.linalg.norm(ts1 - ts2)
